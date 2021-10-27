52f60319f9baf23daf56c79160a3be5cae0cdb69
Evgeniy <devil_98@abv.bg>
2021-10-27 19:31:15 +0300

    [shop]

---
+++ SoftUni/homework/advanced/GUI shop/canvas.py
@@ -0,0 +1,10 @@
+from tkinter import Tk
+
+def create_app():
+    tk = Tk()
+    tk.geometry('700x600')
+    tk.title('GUI Product Shop')
+
+    return tk
+
+tk = create_app()
---
+++ SoftUni/homework/advanced/GUI shop/helpers.py
@@ -0,0 +1,5 @@
+from canvas import tk
+
+def clean_screen():
+    for el in tk.grid_slaves():
+        el.destroy()
