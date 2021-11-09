4c1d52ea6942c3baaf2369a0e99f7d8f49544f13
Evgeniy <devil_98@abv.bg>
2021-11-09 22:54:44 +0200

    [encapsulation]

---
+++ SoftUni/homework/python OOP/4. Encapsulation/mangling_a_method.py
@@ -0,0 +1,17 @@
+class Person:
+
+    def __init__(self):
+        self.first_name = 'Peter'
+        self.last_name = 'Parker'
+
+    def __full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def info(self):
+        return self.__full_name()
+
+
+person = Person()
+print(person.info())
+print(person._Person__full_name())
+print(person.__full_name())
