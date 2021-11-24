8e645e0d58c032e941c0b768301788f276ef5f7d
Evgeniy <devil_98@abv.bg>
2021-11-24 18:47:08 +0200

    [Decorators]

---
+++ SoftUni/homework/python OOP/10. Decorators/1. logged.py
@@ -0,0 +1,12 @@
+def logged(func):
+    def wrapper(*args):
+        res = func(*args)
+        return f'you called {func.__name__}{args}\nit returned {res}'
+
+    return wrapper
+
+
+@logged
+def func(*args):
+    return 3 + len(args)
+print(func(4, 4, 4))
